22e2ad6fb56681b9eda1dea0e2a2930ba8bce6f3
hug5 <dhub2@gmx.com>
2024-05-08 11:46:43 -0700

    init commit

---
+++ start.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+def start():
+
+  flap = Flask(__name__)
+
+  @flap.route("/")
+  def index():
+      return "<p>Hello2</p>"
+
+  #@flap.route("/<arg>")
+  #def subpath(arg):
+  #    return "<p>yahoo2 " + arg + "</p>"
+
+  #@flap.route('/', defaults={'path1': ''})
+  # if you also want to catch root; see URL Route Registrations
+
+  @flap.route('/<path:path1>')
+  def catch_all(path1):
+      return "The path is : %s" %path1
+
+
+  return flap
+
+
+flap = start()
