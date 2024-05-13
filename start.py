df5425baae6af38fba6414b352bde6718056ff03
hug5 <dhub2@gmx.com>
2024-05-13 10:34:14 -0700

    wip

--- start.py
+++ start.py
@@ -2,25 +2,26 @@ from flask import Flask
 
 def start():
 
-  flap = Flask(__name__)
+    flap = Flask(__name__)
 
-  @flap.route("/")
-  def index():
-      return "<p>Hello3</p>"
+    @flap.route("/")
+    def index():
+        return "<p>Hello3</p>"
 
-  #@flap.route("/<arg>")
-  #def subpath(arg):
-  #    return "<p>yahoo2 " + arg + "</p>"
+    #@flap.route("/<arg>")
+    #def subpath(arg):
+    #    return "<p>yahoo2 " + arg + "</p>"
 
-  #@flap.route('/', defaults={'path1': ''})
-  # if you also want to catch root; see URL Route Registrations
+    #@flap.route('/', defaults={'path1': ''})
+    # if you also want to catch root; see URL Route Registrations
 
-  @flap.route('/<path:path1>')
-  def catch_all(path1):
-      return "The path is : %s" %path1
+    @flap.route('/<path:path1>')
+    def catch_all(path1):
+        return "The path is : %s" %path1
 
 
-  return flap
+    return flap
+
 
 
 flap = start()
